d90e2d6a74e640aa05091d266fb68f6d127907ee
Alexander Linder <alexander-linder@gmx.de>
2021-06-23 19:58:32 +0200

    Update sources to final version

--- src/main/java/edu/kit/iti/formal/vser/ByteMeasurable.java
+++ src/main/java/edu/kit/iti/formal/vser/ByteMeasurable.java
@@ -1,16 +1,18 @@
 package edu.kit.iti.formal.vser;
 
+import edu.kit.iti.formal.vser.converters.Integers;
+
 public interface ByteMeasurable {
 
     //@ public instance model int byteSize;
 
     /*@ public normal_behavior
       @
-      @ ensures \result == (\sum int i; 0 <= i && i < measurables.length; measurables[i].byteSize);
+      @ ensures \result == edu.kit.iti.formal.vser.converters.Integers.BYTE_SIZE + (\sum int i; 0 <= i && i < measurables.length; measurables[i].byteSize);
       @ assignable \strictly_nothing;
       @*/
     public static int /*@strictly_pure@*/ byteSize(ByteMeasurable[] measurables) {
-        int result = 0;
+        int result = Integers.BYTE_SIZE;
         /*@
           @ maintaining 0 <= \index && \index < measurables.length;
           @ maintaining result == (\sum int j; 0 <= j && j < \index; measurables[\index].byteSize);
